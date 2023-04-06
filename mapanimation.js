9ced4464a393a5aad023e7fbd501e7475ddcaa4a
Roberto Caroca <63612984+RobertoCaroca@users.noreply.github.com>
2023-04-05 22:01:08 -0400

    Refactor

--- mapanimation.js
+++ mapanimation.js
@@ -1,4 +1,3 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
 const busStops = [
   [-71.093729, 42.359244],
   [-71.094915, 42.360175],
@@ -16,31 +15,49 @@ const busStops = [
 
 // TODO: add your own access token
 mapboxgl.accessToken = 
-'pk.eyJ1Ijoicm9iZXJ0b2Nhcm9jYSIsImEiOiJjbGcyZGM0b3QwNGV4M2txcmo3MDF3MDRsIn0.lY-eikcuAz3QFOlEkCvExA';
+'pk.eyJ1Ijoicm9iZXJ0b2Nhcm9jYSIsImEiOiJjbGcyZGM0b3QwNGV4M2txcmo3MDF3MDRsIn0.lY-eikcuAz3QFOlEkCvExA'; 
 
 // This is the map instance
 let map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-71.104081, 42.365554],
+  center: busStops[0],
   zoom: 14,
 });
 
 // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
+let marker = new mapboxgl.Marker().setLngLat(busStops[0]).addTo(map);
 
 // counter here represents the index of the current bus stop
 let counter = 0;
-function move() {
+let animationActive = false;
+let animation;
+
   // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
   // Use counter to access bus stops in the array busStops
 
-  setTimeout(() => {
-    if (counter >= busStops.length) return;
+  function move() {
+    if (counter >= busStops.length - 1) {
+      counter = 0;
+    } else {
+      counter++;
+    }
     marker.setLngLat(busStops[counter]);
-    counter++;
-    move();
-  }, 1000);
-
-  // Make sure you call move() after you increment the counter.
-}
+    if (animationActive) {
+      animation = setTimeout(move, 1000);
+    }
+  }
+  
+  function startStopAnimation() {
+    if (!animationActive) {
+      animationActive = true;
+      move();
+      document.getElementById("startStopAnimationBtn").textContent = "Pause";
+    } else {
+      animationActive = false;
+      clearTimeout(animation);
+      document.getElementById("startStopAnimationBtn").textContent = "Show Stops";
+    }
+  }
+  
+  document.getElementById("startStopAnimationBtn").addEventListener("click", startStopAnimation);
